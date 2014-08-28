c0a62fb58a5f2624b25586de74c8f90acfbe0d68
rbp <jesusssss>
2014-08-28 11:47:22 +0200

    Initial commit

---
+++ controller/base.controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace controller {
+    class BaseController {
+
+        public function __construct() {
+            echo "This is the base controller";
+        }
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", "1");
+
+echo "Welcome to index. From here we are going to control EVERYTHING";
